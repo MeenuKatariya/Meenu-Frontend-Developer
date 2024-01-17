8b455d8c5773a1758c33f702b4df2875d72a3ef2
Meenu Katariya <katariyameenu963@gmail.com>
2024-01-17 15:55:50 +0530

    grid of capsule complete

--- src/component/Grid.jsx
+++ src/component/Grid.jsx
@@ -4,12 +4,16 @@ export const Grid = ({dataAll}) => {
     // console.log(dataAll)
   return (
     <div>
-        <div style={{ display: "grid", gridTemplateColumns: "auto auto auto" }}>
+        <div className="grid xl:grid-cols-3 gap-10 p-10 md:grid-cols-2 sm:grid-cols-1 ">
         {dataAll?.map((item) => {
           return (
-            <div>
-              <div>{item.capsule_id}</div>
-              <div>{item.capsule_serial}</div>
+            <div className="border-2 border-grey-800 p-7 text-lg">
+              <div><span className="font-bold">Capsule_id: </span>{item.capsule_id}</div>
+              <div> <span className="font-bold">Capsule_Serial: </span>{item.capsule_serial}</div>
+              <div> <span className="font-bold">Status: </span>{item.status}</div>
+              <div> <span className="font-bold">Original_Launch: </span>{item.original_launch}</div>
+              <div> <span className="font-bold">Type: </span>{item.type}</div>
+              <div> <span className="font-bold">Details: </span>{item.details}</div>
             </div>
           );
         })}
